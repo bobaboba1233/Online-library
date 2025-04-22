921026456066a2fd630bb367e6a963d82e201a12
bobaboba1233 <bogdan.shustrov@mail.ru>
2025-04-22 22:57:16 +0300

    Добавил профиль, карточки книг, создал стилевые файлы

--- library/src/App.jsx
+++ library/src/App.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import LibraryLayout from "./LibraryLayout";
+import React from 'react';
+import { Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import BookDetails from './pages/BookDetails';
 
 function App() {
   return (
-    <div>
-      <LibraryLayout />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+      </Routes>
   );
 }
 
